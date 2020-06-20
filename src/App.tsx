7742e4e9e71215cec7d53b45ad101f23bf1bc060
Tomasz Dmowski <snoopy.droid.code@gmail.com>
2020-06-20 13:35:16 +0200

    Imported navigation to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 import { Home } from "./views/Home";
+import {Navigation} from './components/UI/nav/Navigation'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export const App: React.FC = () => {
   return (
     <Router>
-      <div className="App">
-        <Home />
-      </div>
+      <header id="main-header">
+        <div className="container">
+          <Navigation/>
+          <Home />
+        </div>
+      </header>
     </Router>
   );
 };
