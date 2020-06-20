097fecaac53afa42d4b765c2763e3e9c817e4e3a
Tomasz Dmowski <snoopy.droid.code@gmail.com>
2020-06-20 13:38:34 +0200

    Style app component with import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import './app.css'
 import { Home } from "./views/Home";
 import {Navigation} from './components/UI/nav/Navigation'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
