069cd3e2284c4c83fcf2462c7a6866e5b7a7e648
Tomasz Dmowski <snoopy.droid.code@gmail.com>
2020-06-20 23:29:54 +0200

    import css to Projects view

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Modal } from "../components/UI/Modal";
 import firebase from "../firebase";
+import './styles/projects.css'
 
 export const Projects = () => {
   const [project, setProject] = useState<any>([]);
