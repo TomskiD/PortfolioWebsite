d4c6d8902f14f184a813acb426f8bd1ed20a800e
Tomasz Dmowski <snoopy.droid.code@gmail.com>
2020-06-20 11:45:08 +0200

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
+import {Button} from './components/Button'
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Button text='See projects'/>
     </div>
   );
 };
